c86b7dfc7f24842b1fbb4b7cb941333035e80feb
Ben Kogan <benkogan9@gmail.com>
2024-06-02 00:52:24 -0700

    stage 7

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"slices"
 	"strings"
 )
@@ -25,13 +26,15 @@ func run() error {
 		}
 		command = strings.TrimSpace(command)
 
-		handleCommand(command)
+		if err := handleCommand(command); err != nil {
+			return fmt.Errorf("error handling command: %w", err)
+		}
 	}
 }
 
 var BUILTINS = []string{"exit", "echo", "type"}
 
-func handleCommand(command string) {
+func handleCommand(command string) error {
 	parts := strings.Split(command, " ")
 	switch parts[0] {
 	case "exit":
@@ -41,10 +44,42 @@ func handleCommand(command string) {
 	case "type":
 		if slices.Contains(BUILTINS, parts[1]) {
 			fmt.Printf("%s is a shell builtin\n", parts[1])
+			return nil
+		}
+
+		path, _ := os.LookupEnv("PATH")
+		commandPath, err := locateCommand(parts[1], strings.Split(path, ":"))
+		if err != nil {
+			return fmt.Errorf("error locating command: %w", err)
+		}
+
+		if commandPath != "" {
+			fmt.Printf("%s is %s\n", parts[1], commandPath)
 		} else {
-			fmt.Printf("%s not found\n", parts[1])
+			fmt.Printf("%s: command not found\n", parts[1])
 		}
 	default:
 		fmt.Printf("%s: command not found\n", command)
 	}
+	return nil
+}
+
+func locateCommand(command string, path []string) (string, error) {
+	for _, dir := range path {
+		var found string
+		err := filepath.WalkDir(dir, func(path string, d os.DirEntry, err error) error {
+			if !d.IsDir() && d.Name() == command {
+				found = path
+				return filepath.SkipDir
+			}
+			return nil
+		})
+		if err != nil {
+			return "", fmt.Errorf("error walking directory: %w", err)
+		}
+		if found != "" {
+			return found, nil
+		}
+	}
+	return "", nil
 }
